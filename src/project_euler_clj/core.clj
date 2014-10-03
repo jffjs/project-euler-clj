95fc9ec1d61d9c7b14343f9cdddf21d5c4a3f3fc
jffjs <jffreyjs@gmail.com>
2014-10-03 15:54:09 -0400

    problem 2

--- src/project_euler_clj/core.clj
+++ src/project_euler_clj/core.clj
@@ -5,3 +5,11 @@
   (reduce + (filter #(or (= (mod % 3) 0)
                          (= (mod % 5) 0))
                     (range 1 1000))))
+
+;; Problem 2
+(defn fib
+  ([] (fib 1 1))
+  ([n1 n2] (cons n1 (lazy-seq (fib n2 (+ n2 n1))))))
+
+(defn problem-2 []
+  (reduce + (filter even? (take-while (partial > 4000000) (fib)))))
